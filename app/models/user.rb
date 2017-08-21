a0f1482b4bb2c822eb7bc9118558b2716f73f2ce
Carlos Omana <araogaraog@gmail.com>
2017-08-20 20:13:22 -0500

    Add devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@
 # first_name:string
 # last_name:string
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true
 end
